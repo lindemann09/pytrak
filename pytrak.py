ec1445f829d2486533ad042bd836785b25670e25
Raphael Wallroth <rwallrot@uni-potsdam.de>
2014-06-12 17:29:05 +0200

    coloured dot visualisation

--- pytrak.py
+++ pytrak.py
@@ -1,5 +1,5 @@
 from trakstar import TrakSTARInterface
-from expyriment import control, stimuli, design, io
+from expyriment import control, stimuli, design, io, misc
 import time
 
 
@@ -28,7 +28,17 @@ if not write_angle and not write_quality:
     output.add_variable_names(["time", "sensor", "x", "y", "z"])
 #elif ... 
 
-init_time = time.time()
+colours = { 0: misc.constants.C_RED,
+            1: misc.constants.C_GREEN,
+            2: misc.constants.C_YELLOW,
+            3: misc.constants.C_BLUE }
+circle_diameter = 20
+circles = {}
+canvas = stimuli.BlankScreen()
+for sensor in sensors:
+    circles[sensor] = stimuli.Circle(circle_diameter, colour=colours[sensor])
+
+init_time = time.time() 
 
 exp.keyboard.clear()
 exp.clock.reset_stopwatch()
@@ -38,14 +48,18 @@ while key is None:
     for sensor in sensors:
         output.add([data["time"], sensor, data[sensor][0],
                     data[sensor][1], data[sensor][2]])
-    
+        circles[sensor].position = (int(round(data[sensor][1]*10)),
+                                    int(round(data[sensor][0]*10)))
+    canvas.clear_surface()
     sample_rate = 1000 * cnt / float(exp.clock.stopwatch_time)
-    if cnt % 30==0:
-        stimuli.TextBox(text = "{0}\n{1}\n".format(cnt, sample_rate) +
-                        TrakSTARInterface.data2string(data),
-                        size = (400, 300), text_size=20).present()
-
-    
+    #if cnt % 30==0:
+    stimuli.TextBox(text = "{0}\n{1}\n".format(cnt, sample_rate) +
+                    TrakSTARInterface.data2string(data),
+                    size = (400, 300), text_size=20).plot(canvas)
+    for circle in circles.values():
+        circle.plot(canvas)
+    canvas.present()
+                
     output.save()        
     key = exp.keyboard.check()
     
