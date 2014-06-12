15288e488a5333a9ee24e0246492126c701b9c15
Raphael Wallroth <rwallrot@uni-potsdam.de>
2014-06-12 16:47:15 +0200

    write data to file

--- pytrak.py
+++ pytrak.py
@@ -1,5 +1,5 @@
 from trakstar import TrakSTARInterface
-from expyriment import control, stimuli, design
+from expyriment import control, stimuli, design, io
 
 
 control.defaults.initialize_delay = 0
@@ -18,20 +18,34 @@ exp.keyboard.wait()
 
 key = None
 cnt = 0
+
+sensors = trakstar.attached_sensors
+output = io.DataFile("trakStar_data")
+write_angle = False
+write_quality = False
+if not write_angle and not write_quality:
+    output.add_variable_names(["time", "sensor", "x", "y", "z"])
+#elif ... 
+
 exp.keyboard.clear()
 exp.clock.reset_stopwatch()
 while key is None:
     cnt += 1
     data = trakstar.getSynchronousRecordDataDict()
+    for sensor in sensors:
+        output.add([data["time"], sensor, data[sensor][0],
+                    data[sensor][1], data[sensor][2]])
     
     sample_rate = 1000 * cnt / float(exp.clock.stopwatch_time)
     if cnt % 30==0:
         stimuli.TextBox(text = "{0}\n{1}\n".format(cnt, sample_rate) +
                         TrakSTARInterface.data2string(data),
                         size = (400, 300), text_size=20).present()
+    output.save()        
     key = exp.keyboard.check()
     
 stimuli.TextLine(text="Closing trakSTAR").present()
 trakstar.close()
 control.end()
+
 
