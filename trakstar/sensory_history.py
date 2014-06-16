28a77576216f6cda61298f97362df967ec0cc9cf
Oliver Lindemann <lindemann09@googlemail.com>
2014-06-16 12:10:14 +0200

    SensoryHistory velocity

--- trakstar/sensory_history.py
+++ trakstar/sensory_history.py
@@ -1,14 +1,10 @@
-"""Sensor History with moving average filtering and replacement methods"""
+"""Sensor History with moving average filtering and distance, velocity"""
 
 __author__ = 'Oliver Lindemann <oliver.lindemann@cognitive-psychology.eu>,\
 Raphael Wallroth <>'
 __version__ = 0.2
 
 import math
-
-def euclidian_distance(a, b):
-    """calculates euclidian distance between a and b"""
-    return math.sqrt(sum( map(lambda x:(x[1]-x[0])**2, zip(a, b)) ))
 
 class SensorHistory():
     """The Sensory History keeps track of the last n recorded sample and
@@ -19,6 +15,7 @@ class SensorHistory():
         self.history = [[0] * number_of_sensors] * history_size
         self._moving_average = [0] * number_of_sensors
         self._correction_cnt = 0
+        self._previous_moving_average = self._moving_average
 
     def __str__(self):
         return str(self.history)
@@ -34,6 +31,7 @@ class SensorHistory():
 
         """
 
+        self._previous_moving_average = self._moving_average
         pop = self.history.pop(0)
         self.history.append(values)
         # pop first element and calc moving average
@@ -61,6 +59,18 @@ class SensorHistory():
             s = map(lambda x:x[0]+x[1], zip(s, t))
         return map(lambda x:x/len(self.history), s)
 
+    def distance_to_point(self, point):
+        """returns current euclidian distance to a point in space, based on
+        filtered data (moving average)
+
+        Note
+        ----
+        point has to match in the number of dimensions
+        """
+
+        return math.sqrt(sum( map(lambda x:(x[1]-x[0])**2,
+                    zip(self._moving_average, point)) ))
+
     @property
     def history_size(self):
         return len(self.history)
@@ -74,14 +84,17 @@ class SensorHistory():
         return self._moving_average
 
     @property
-    def replacement(self):
-        """returns the last replacement"""
-        return map(lambda x:x[0]-x[1], zip(self.history[-1], self.history[-2]))
+    def previous_moving_average(self):
+        return self._previous_moving_average
 
     @property
-    def replacement_distance(self):
-        """ returns the euclidian distance of the last replacement"""
-        return euclidian_distance(self.history[-1], self.history[-2])
+    def replacement(self):
+        """returns the current replacement based on filtered data"""
+        return map(lambda x:x[0]-x[1], zip(self._moving_average,
+                self._previous_moving_average))
+    def velocity(self, sampling_rate):
+        """returns the current velocity based on filtered data"""
+        return self.distance_to_point(self._previous_moving_average) * sampling_rate
 
 if __name__ == "__main__":
     import random
@@ -92,3 +105,4 @@ if __name__ == "__main__":
         sh.update(x)
 
     print sh.moving_average, sh.calc_history_average()
+    print sh.velocity(100)
