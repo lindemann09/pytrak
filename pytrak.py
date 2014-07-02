d9d2589e1758b71e5c76725ad11f5c199712fcf0
Oliver Lindemann <lindemann09@googlemail.com>
2014-07-02 18:35:09 +0200

    clean up code

--- pytrak.py
+++ pytrak.py
@@ -10,7 +10,7 @@ from plotter3d import Plotter3d
 
 __author__ = 'Raphael Wallroth <>, \
 Oliver Lindemann <oliver.lindemann@cognitive-psychology.eu>'
-__version__ = '0.1.2'
+__version__ = '0.2'
 
 
 trakstar = TrakSTARInterface()
@@ -50,126 +50,13 @@ exp.set_log_level(0)
 control.initialize(exp)
 
 
-t_wait = 1500
-def invalid_value():
-    stimuli.TextLine(text="Invalid value!").present()
-    exp.clock.wait(t_wait)
-
-def get_input(i):
-    global measurement_rate, max_range, report_rate, power_line, metric
-    if i == 1:
-        measurement_rate = int(io.TextInput("Measurement rate (20 Hz < rate < 255 Hz):", length=3,
-                                            ascii_filter=range(ord("0"),ord("9")+1)).get())
-        if measurement_rate < 20 or measurement_rate > 255:
-            measurement_rate = 80
-            invalid_value()
-        else:
-            stimuli.TextLine(text="Measurement rate set to {0} Hz.".format(measurement_rate)).present()
-            exp.clock.wait(t_wait)
-        return
-    elif i == 2:
-        max_range = int(io.TextInput("Maximum range of transmitter (36, 72, 144 inches):", length=3,
-                                     ascii_filter=range(ord("1"),ord("5"))+[ord("6"),ord("7")]).get())
-        if max_range not in [36, 72, 144]:
-            max_range = 36
-            invalid_value()
-        else:
-            stimuli.TextLine(text="Maximum range of transmitter set to {0} inches.".format(max_range)).present()
-            exp.clock.wait(t_wait)
-        return
-    elif i == 3:
-        report_rate = int(io.TextInput("Report rate of the data (1 <= rate <= 127):", length=3,
-                                       ascii_filter=range(ord("0"),ord("9")+1)).get())
-        if report_rate < 1 or report_rate > 127:
-            report_rate = 1
-            invalid_value()
-        else:
-            stimuli.TextLine(text="Report rate of incoming data set to {0}.".format(report_rate)).present()
-            exp.clock.wait(t_wait)
-        return
-    elif i == 4:
-        power_line = int(io.TextInput("Power line frequency of the AC power source (50 or 60 Hz):",
-                                      length=2, ascii_filter=[ord("0"),ord("5"),ord("6")]).get())
-        if power_line not in [50, 60]:
-            power_line = 60
-            invalid_value()
-        else:
-            stimuli.TextLine(text="Power line frequency of the AC power source set to {0} Hz.".format(power_line)).present()
-            exp.clock.wait(t_wait)
-        return
-    elif i == 5:
-        metric = int(io.TextInput("Switch metric data reporting on/off (1/0):",
-                                  length=1, ascii_filter=[ord("0"),ord("1")]).get())
-        if metric not in [0, 1]:
-            metric = True
-            invalid_value()
-        else:
-            stimuli.TextLine(text="Metric data reporting set to {0}.".format(metric)).present()
-            exp.clock.wait(t_wait)
-        return
-
-def get_udp_input(s):
-    """
-    Input received via udp to change TrakSTAR system settings.
-    Every string begins with a description of the value to be changed,
-    followed by a colon, and the value to be set.
-
-    e.g.: "measurement: 50", "metric: 0"
-    """
-    global filename, measurement_rate, max_range, report_rate, power_line, metric
-    if s.lower().startswith("filename"):
-        _, filename = s.split(":")
-        filename = filename.strip()
-        return
-    elif s.lower().startswith("measurement"):
-        _, measurement_rate = s.split(":")
-        measurement_rate = int(measurement_rate.strip())
-        if measurement_rate < 20 or measurement_rate > 255:
-            measurement_rate = 80
-        return
-    elif s.lower().startswith("maximum"):
-        _, max_range = s.split(":")
-        max_range = int(max_range.strip())
-        if max_range not in [36, 72, 144]:
-            max_range = 36
-        return
-    elif s.lower().startswith("report"):
-        _, report_rate = s.split(":")
-        report_rate = int(report_rate.strip())
-        if report_rate < 1 or report_rate > 127:
-            report_rate = 1
-        return
-    elif s.lower().startswith("power"):
-        _, power_line = s.split(":")
-        power_line = int(power_line.strip())
-        if power_line not in [50, 60]:
-            power_line = 60
-        return
-    elif s.lower().startswith("metric"):
-        _, metric = s.split(":")
-        metric = int(metric.strip())
-        if metric not in [0, 1]:
-            metric = True
-        return
-
-menu = stimuli.TextScreen("Settings:",
-                          "1: Measurement rate\n"+
-                          "2: Maximum range of transmitter\n"+
-                          "3: Report rate of data\n"+
-                          "4: Power line frequency\n"+
-                          "5: Metric data reporting\n\n"+
-                          "q: Quit settings",
-                          text_justification=0,
-                          size=[sz[0]/4, sz[1]/2])
-
-filename, measurement_rate, max_range, report_rate, power_line, metric = '', 80, 36, 1, 60, True
-
 stimuli.TextLine(text="Use remote control? (Y/N)").present()
 key = exp.keyboard.wait([ord("z"), ord("y"), ord("n")])[0]
 
 #remote control
 if key == ord("y") or key == ord("z"):
     remote = True
+    filename = ""
     trakstar.udp.poll_last_data() # clear buffer
     while not trakstar.udp.is_connected: # wait for connection
         stimuli.TextLine(text="Waiting for connection...").present()
@@ -201,9 +88,11 @@ else:
 
 
 #set system settings
-trakstar.set_system_configuration(measurement_rate=measurement_rate,
-                                  max_range=max_range, power_line=power_line,
-                                  metric=metric, report_rate=report_rate,
+trakstar.set_system_configuration(measurement_rate=settings.measurement_rate,
+                                  max_range=settings.max_range,
+                                  power_line=settings.power_line,
+                                  metric=settings.metric,
+                                  report_rate=settings.report_rate,
                                   print_configuration=True)
 
 trakstar.open_data_file(filename=filename, directory="data",
@@ -221,22 +110,21 @@ for sensor in trakstar.attached_sensors:
                      text_colour=misc.constants.C_WHITE).plot(circles[sensor])
     history[sensor] = SensorHistory(history_size=5, number_of_parameter=3)
 
-info_col = (255, 150, 50)
-txt_v = stimuli.TextLine("v: visualize sensors", position=[-sz[0]/2+100, -sz[1]/2+50],
-                         text_size=15, text_colour=info_col)
-txt_p = stimuli.TextLine("p: pause/unpause", position=[0, -sz[1]/2+50],
-                         text_size=15, text_colour=info_col)
-txt_q = stimuli.TextLine("q: quit recording", position=[sz[0]/2-100, -sz[1]/2+50],
-                         text_size=15, text_colour=info_col)
-txt_fn = stimuli.TextLine("filename: "+filename, position=[-sz[0]/2+100, sz[1]/2-50],
-                            text_size=15, text_colour=info_col)
-txt_date = stimuli.TextLine(text = "date: {0}".format(strftime("%d/%m/%Y")),
-                            position=[sz[0]/2-100, sz[1]/2-50],
-                            text_size=15, text_colour=info_col)
+# make text lines
+def text_line(text, position, text_size=15, text_colour = (255, 150, 50)):
+    return stimuli.TextLine(text_line, position = position,
+                    text_size = text_size, text_colour=text_colour)
+sz = control.defaults.window_size
+txt_v = text_line("v: visualize sensors", [-sz[0]/2+100, -sz[1]/2+50])
+txt_p = text_line("p: pause/unpause", [0, -sz[1]/2+50])
+txt_q = text_line("q: quit recording", [sz[0]/2-100, -sz[1]/2+50])
+txt_fn = text_line(("filename: " + settings.filename,
+                        position=[-sz[0]/2+100, sz[1]/2-50])
+txt_date = text_line("date: {0}".format(strftime("%d/%m/%Y")),
+                            position=[sz[0]/2-100, sz[1]/2-50])
 info_txts = [txt_v, txt_p, txt_q, txt_fn, txt_date]
 
-txt_pause = stimuli.TextLine("PAUSED", position=[0,-50],text_size=50, text_colour=info_col)
-canvas = stimuli.BlankScreen()
+txt_pause = text_line("PAUSED", position=[0,-50], text_size=50))
 
 
 def update_circles(trakstar,circles, history):
@@ -251,18 +139,17 @@ def update_circles(trakstar,circles, history):
     for circle in circles.values(): #todo
         circle.plot(canvas)
 
-def show_info_screen():
+def show_info_screen(pause = False):
     canvas = stimuli.BlankScreen()
     for txt in info_txts:
         txt.plot(canvas)
+    if pause:
+        txt_pause.plot(canvas)
     canvas.present()
 
 
 pause = False
 cnt = -1
-div = report_rate
-if report_rate > 40:
-    div = 40
 exp.keyboard.clear()
 
 if remote:
@@ -285,7 +172,7 @@ while True:
     if not pause:
         cnt += 1
         data = trakstar.get_synchronous_data_dict() #polls udp while unpaused
-        udp_input = data['udp'] #temporary variable to check for udp input read in by trakstar.get_synchronous_data_dict() 
+        udp_input = data['udp'] #temporary variable to check for udp input read in by trakstar.get_synchronous_data_dict()
 
         exp.clock.reset_stopwatch()
         #for sensor in trakstar.attached_sensors:
@@ -293,41 +180,44 @@ while True:
         plotter.update_values(data)
         exp.screen.update_stimuli(plotter.plotter_array)
         print exp.clock.stopwatch_time
-        
-        if cnt % (40/div) == 0 and show_circles:
-            update_circles(trakstar, circles, history)
+
+        #if cnt % (40) == 0 and show_circles:
+        #    update_circles(trakstar, circles, history)
     else:
         if remote:
             if udp_input.lower() == 'pause':
-                txt_pause.plot(canvas)
-                canvas.present()
+                pause = True
+                show_info_screen(pause = pause)
                 udp_input = '0' #reset the pause command to None
             s = trakstar.udp.poll() #polls udp while paused
             if s is not None and s.lower() == 'unpause':
                 pause = False
+                show_info_screen(pause = False)
                 trakstar.udp.send('confirm')
                 #if you wish to have the 'unpause' command in your data output, you'll have to send it twice!
-        
+
     key = exp.keyboard.check()
     if key == ord("v"):
         show_circles = not show_circles
     elif key == ord("p"):
         pause = not pause
+        show_info_screen(pause = pause)
     elif key == ord("q"):
         break
     elif key == misc.constants.K_UP:
         plotter.scale += 0.01
     elif key == misc.constants.K_DOWN:
         plotter.scale -= 0.01
-    
+
     if remote:
         if udp_input != '0':
             if udp_input.lower() == 'pause':
-                pause = True                
+                pause = True
                 trakstar.udp.send('confirm')
+                show_info_screen(pause = pause)
             elif udp_input.lower() == 'quit':
                 trakstar.udp.send('confirm')
-                break     
+                break
 
 
 
--- settings.py
+++ settings.py
@@ -1,11 +1,181 @@
-from expyriment import misc
+""" Settings module for pytrak
 
-colours = { 1: misc.constants.C_RED,
-            2: misc.constants.C_GREEN,
-            3: misc.constants.C_YELLOW,
-            4: misc.constants.C_BLUE }
+    reads and save settings automatically
+"""
+
+import atexit
+import ConfigParser
+from expyriment import stimuli, misc
+
+### settings ###
+measurement_rate = 80
+max_range = 36
+report_rate = 1
+power_line = 60
+metric = True
+
 circle_diameter = 40
 
 plotter_width = 500
 plotter_background_colour = (0,0,0)
 plotter_axis_colour = (100,100,100)
+####
+
+
+colours = { 1: misc.constants.C_RED,
+            2: misc.constants.C_GREEN,
+            3: misc.constants.C_YELLOW,
+            4: misc.constants.C_BLUE }
+
+t_wait = 1500
+cfg_filename = "pytrak.cfg"
+cfg_section = 'TrakStar'
+
+def save():
+    config = ConfigParser.RawConfigParser()
+    config.add_section(cfg_section)
+    config.set(cfg_section, "measurement_rate", measurement_rate)
+    config.set(cfg_section, "max_range", max_range)
+    config.set(cfg_section, "report_rate", report_rate)
+    config.set(cfg_section, "power_line", power_line)
+    config.set(cfg_section, "circle_diameter", circle_diameter)
+    config.set(cfg_section, "metric", metric)
+    with open(cfg_filename, 'wb') as configfile:
+        config.write(configfile)
+
+def read():
+    global measurement_rate, max_range
+    global report_rate, power_line, metric, circle_diameter
+
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(cfg_filename)
+        measurement_rate = config.getint(cfg_section, 'measurement_rate')
+        max_range = config.getint(cfg_section, 'max_range')
+        report_rate = config.getint(cfg_section, 'report_rate')
+        power_line = config.getint(cfg_section, 'power_line')
+        circle_diameter = config.getint(cfg_section, 'circle_diameter')
+        metric = config.getboolean(cfg_section, 'metric')
+    except:
+        print "Creating settings file: ", cfg_filename
+        save()
+        return False
+    return True
+
+def get_menu(exp):
+    return stimuli.TextScreen("Settings:",
+                          "1: Measurement rate\n"+
+                          "2: Maximum range of transmitter\n"+
+                          "3: Report rate of data\n"+
+                          "4: Power line frequency\n"+
+                          "5: Metric data reporting\n\n"+
+                          "q: Quit settings",
+                          text_justification=0,
+                          size=[exp.screen.size[0]/4,
+                                exp.screen.size[1]/2])
+
+def invalid_value(exp):
+    stimuli.TextLine(text="Invalid value!").present()
+    exp.clock.wait(t_wait)
+
+def get_input(exp, i):
+    global measurement_rate, max_range, report_rate, power_line, metric
+    if i == 1:
+        measurement_rate = int(io.TextInput("Measurement rate (20 Hz < rate < 255 Hz):", length=3,
+                                            ascii_filter=range(ord("0"),ord("9")+1)).get())
+        if measurement_rate < 20 or measurement_rate > 255:
+            measurement_rate = 80
+            invalid_value()
+        else:
+            stimuli.TextLine(text="Measurement rate set to {0} Hz.".format(measurement_rate)).present()
+            exp.clock.wait(t_wait)
+        return
+    elif i == 2:
+        max_range = int(io.TextInput("Maximum range of transmitter (36, 72, 144 inches):", length=3,
+                                     ascii_filter=range(ord("1"),ord("5"))+[ord("6"),ord("7")]).get())
+        if max_range not in [36, 72, 144]:
+            max_range = 36
+            invalid_value()
+        else:
+            stimuli.TextLine(text="Maximum range of transmitter set to {0} inches.".format(max_range)).present()
+            exp.clock.wait(t_wait)
+        return
+    elif i == 3:
+        report_rate = int(io.TextInput("Report rate of the data (1 <= rate <= 127):", length=3,
+                                       ascii_filter=range(ord("0"),ord("9")+1)).get())
+        if report_rate < 1 or report_rate > 127:
+            report_rate = 1
+            invalid_value()
+        else:
+            stimuli.TextLine(text="Report rate of incoming data set to {0}.".format(report_rate)).present()
+            exp.clock.wait(t_wait)
+        return
+    elif i == 4:
+        power_line = int(io.TextInput("Power line frequency of the AC power source (50 or 60 Hz):",
+                                      length=2, ascii_filter=[ord("0"),ord("5"),ord("6")]).get())
+        if power_line not in [50, 60]:
+            power_line = 60
+            invalid_value()
+        else:
+            stimuli.TextLine(text="Power line frequency of the AC power source set to {0} Hz.".format(power_line)).present()
+            exp.clock.wait(t_wait)
+        return
+    elif i == 5:
+        metric = int(io.TextInput("Switch metric data reporting on/off (1/0):",
+                                  length=1, ascii_filter=[ord("0"),ord("1")]).get())
+        if metric not in [0, 1]:
+            metric = True
+            invalid_value()
+        else:
+            stimuli.TextLine(text="Metric data reporting set to {0}.".format(metric)).present()
+            exp.clock.wait(t_wait)
+        return
+
+def get_udp_input(exp, s):
+    """
+    Input received via udp to change TrakSTAR system settings.
+    Every string begins with a description of the value to be changed,
+    followed by a colon, and the value to be set.
+
+    e.g.: "measurement: 50", "metric: 0"
+    """
+    global filename, measurement_rate, max_range, report_rate, power_line, metric
+    if s.lower().startswith("filename"):
+        _, filename = s.split(":")
+        filename = filename.strip()
+        return
+    elif s.lower().startswith("measurement"):
+        _, measurement_rate = s.split(":")
+        measurement_rate = int(measurement_rate.strip())
+        if measurement_rate < 20 or measurement_rate > 255:
+            measurement_rate = 80
+        return
+    elif s.lower().startswith("maximum"):
+        _, max_range = s.split(":")
+        max_range = int(max_range.strip())
+        if max_range not in [36, 72, 144]:
+            max_range = 36
+        return
+    elif s.lower().startswith("report"):
+        _, report_rate = s.split(":")
+        report_rate = int(report_rate.strip())
+        if report_rate < 1 or report_rate > 127:
+            report_rate = 1
+        return
+    elif s.lower().startswith("power"):
+        _, power_line = s.split(":")
+        power_line = int(power_line.strip())
+        if power_line not in [50, 60]:
+            power_line = 60
+        return
+    elif s.lower().startswith("metric"):
+        _, metric = s.split(":")
+        metric = int(metric.strip())
+        if metric not in [0, 1]:
+            metric = True
+        return
+
+###
+atexit.register(save)
+read()
+###
