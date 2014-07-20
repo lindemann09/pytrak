9064337539b346a12eb6fd1add34bececd2a7af3
Oliver Lindemann <lindemann09@googlemail.com>
2014-07-20 15:00:47 +0200

    minor

--- pytrak/trakstar/__init__.py
+++ pytrak/trakstar/__init__.py
@@ -2,7 +2,6 @@
 
 __author__ = 'Raphael Wallroth <rwallroth@uni-potsdam.de>, \
 Oliver Lindemann <oliver.lindemann@cognitive-psychology.eu>'
-__version__ = 0.3
 
 import atc3dg_functions as api
 from trakstar_interface import *
